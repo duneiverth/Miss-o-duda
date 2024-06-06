1e8b734c566dadc52607876a71517c3874cffa91
Duda <105284664+duneiverth@users.noreply.github.com>
2024-06-06 10:33:46 -0300

    ok

---
+++ script.js
@@ -0,0 +1,18 @@
+const caixaPrincipal = document.querySelector(".caixa-principal")
+const caixaPerguntas = document.querySelector(".caixa-pergunta")
+const caixaAlternativa = document.querySelector(".caixa-alternariva")
+const caixaResultado = document.querySelector(".caixa-resultado")
+const caixaResultado = document.querySelector(".texto-resultado")
+
+const perguntas = [
+    { //primeiro         objetoda lista e perguntas
+        enunciado: "pergunta1",
+        alternativas: ["alternativa1", "alternativa2"]
+    },
+    {
+        enunciado: "pergunta2",
+        alternativas: ["alternativa1", "alternativa2"]
+    }
+        
+]
+
